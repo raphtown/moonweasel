6806f94cd7553dd08336c18aab255d8a99797981
Maxime Serrano <mserrano@Thibs-iMac-11.local>
2010-01-14 10:24:11 -0800

    Cleanup

--- src/org/atcs/moonweasel/rmi/Client.java
+++ src/org/atcs/moonweasel/rmi/Client.java
@@ -1,21 +1,21 @@
 package org.atcs.moonweasel.rmi;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.rmi.Naming;
 
 public class Client {
-    public static void main(String args[]) {
-        if (System.getSecurityManager() == null) {
+    public static void main(String args[])
+    {
+        if (System.getSecurityManager() == null)
             System.setSecurityManager(new SecurityManager());
-        }
-        try {
-            String name = "Compute";
-            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 4001);
-            Simulator comp = (Simulator) registry.lookup(name);
+
+        try
+        {
+            Simulator comp = (Simulator) Naming.lookup("Simulator");
             int pi = comp.doStuff();
             System.out.println(pi);
-        } catch (Exception e) {
-            System.err.println("ComputePi exception:");
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     }    
--- src/org/atcs/moonweasel/rmi/Server.java
+++ src/org/atcs/moonweasel/rmi/Server.java
@@ -1,35 +1,27 @@
 package org.atcs.moonweasel.rmi;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server implements Simulator {
 
     public static void main(String[] args) {
-        if (System.getSecurityManager() == null) {
+        if (System.getSecurityManager() == null)
             System.setSecurityManager(new SecurityManager());
-            System.getSecurityManager().checkAccept("127.0.0.1", 4001);
-            System.getSecurityManager().checkAccept("172.30.24.61", 4001);
-        }
+
         try {
-            String name = "Simulator";
             Server engine = new Server();
-            Simulator stub =
-                (Simulator) UnicastRemoteObject.exportObject(engine, 4001);
-            Registry registry = LocateRegistry.getRegistry();
-            registry.rebind(name, stub);
+            Simulator stub = (Simulator) UnicastRemoteObject.exportObject(engine, 4001);
+            Naming.rebind("Simulator", stub);
             System.out.println("Simulator bound");
         } catch (Exception e) {
-            System.err.println("ComputeEngine exception:");
             e.printStackTrace();
         }
     }
 
 	public int doStuff() throws RemoteException
 	{
-		// TODO Auto-generated method stub
 		return 0;
 	}
 }
