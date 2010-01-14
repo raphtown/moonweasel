ed85be0216fb39db1e6b6c2f230889f7885ba5cd
Maxime Serrano <mserrano@Thibs-iMac-11.local>
2010-01-14 10:30:56 -0800

    Removed the need for useless jjunk.

--- src/org/atcs/moonweasel/rmi/Server.java
+++ src/org/atcs/moonweasel/rmi/Server.java
@@ -2,16 +2,19 @@ package org.atcs.moonweasel.rmi;
 
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server implements Simulator {
 
+	private static int stuff = 0;
     public static void main(String[] args) {
         if (System.getSecurityManager() == null)
             System.setSecurityManager(new SecurityManager());
 
         try {
             Server engine = new Server();
+            LocateRegistry.createRegistry(1099);
             Simulator stub = (Simulator) UnicastRemoteObject.exportObject(engine, 4001);
             Naming.rebind("Simulator", stub);
             System.out.println("Simulator bound");
@@ -22,6 +25,6 @@ public class Server implements Simulator {
 
 	public int doStuff() throws RemoteException
 	{
-		return 0;
+		return stuff++;
 	}
 }
