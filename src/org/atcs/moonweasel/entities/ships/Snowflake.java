15a3fe3518787d290cf0b1c61b2f9c419f9aae92
Drew Schleck <drew.schleck@menloschool.org>
2010-01-20 08:23:46 -0800

    Added a temporary draw method.

--- src/org/atcs/moonweasel/entities/ships/Snowflake.java
+++ src/org/atcs/moonweasel/entities/ships/Snowflake.java
@@ -1,15 +1,25 @@
 package org.atcs.moonweasel.entities.ships;
 
+import javax.media.opengl.GL2;
+
+import com.sun.opengl.util.gl2.GLUT;
+
 public class Snowflake extends Ship {
 	public static final String NAME = "snowflake";
 	
 	private static final ShipData DATA = 
 		ShipData.loadShipData(Snowflake.class.getSimpleName().toLowerCase());
+	private static GLUT glut = new GLUT();
 	
 	private long time;
 	
 	private Snowflake() {
 		super(DATA);
+	}
+	
+	@Override
+	public void draw(GL2 gl) {
+		glut.glutSolidCube(10);
 	}
 	
 	@Override
