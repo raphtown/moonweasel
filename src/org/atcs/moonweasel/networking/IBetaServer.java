859b879024c321ff0b2fccc71b9c4f2f0950d9f6
Raphael Townshend <raphael@tc.com>
2010-01-27 08:56:15 -0800

    Started writing beta classes for testing reflection.

---
+++ src/org/atcs/moonweasel/networking/IBetaServer.java
@@ -0,0 +1,22 @@
+package org.atcs.moonweasel.networking;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import org.atcs.moonweasel.entities.Entity;
+
+/**
+ * The interface linked to being a server. Used only for the purpose of keeping 
+ * RMI happy.
+ * @author Maxime Serrano, Raphael Townshend
+ */
+public interface IBetaServer extends Remote
+{
+    /**
+	 * When the client sends in a command, call this method.
+	 * @param command The command(s) that have been pressed.
+	 * @param c The client that is using this command.
+	 */
+	public Object sendPacket(String command, String c) throws RemoteException;
+}
