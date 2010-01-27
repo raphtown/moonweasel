999221ca63e1b29a648fffd327a7fa08d9dbd243
Maxime Serrano <mserrano@Thibs-iMac-11.local>
2010-01-27 08:00:29 -0800

    Forgot the deleted files.

--- src/org/atcs/moonweasel/networking/Networking.java
+++
@@ -1,8 +0,0 @@
-package org.atcs.moonweasel.networking;
-
-import org.atcs.moonweasel.Destructible;
-
-public interface Networking extends Destructible
-{
-	public void update();
-}
--- src/org/atcs/moonweasel/rmi/IClient.java
+++
@@ -1,9 +0,0 @@
-package org.atcs.moonweasel.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IClient extends Remote
-{
-	public void forceUpdate() throws RemoteException;
-}
