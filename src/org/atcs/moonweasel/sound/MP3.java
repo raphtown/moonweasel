6441a3c8c1a923a39e7ba2ea0254456064940a7a
Frans Honig <franshonig@frans-honigs-macbook-pro.local>
2010-02-04 11:38:19 -0800

    Sound System Implemented New Ships

---
+++ src/org/atcs/moonweasel/sound/MP3.java
@@ -0,0 +1,70 @@
+package org.atcs.moonweasel.sound;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+import javazoom.jl.player.Player;
+
+
+public class MP3 {
+    private String filename="data/music/music.mp3";
+    private Player player; 
+
+    // constructor that takes the name of an MP3 file
+    public MP3(String filename) 
+    {
+    	
+    	this.filename = filename;
+
+    }
+
+    public void close() { if (player != null) player.close(); }
+
+    // play the MP3 file to the sound card
+    public void play() {
+        try {
+            FileInputStream fis     = new FileInputStream(filename);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            player = new Player(bis);
+            
+        }
+        catch (Exception e) {
+            System.out.println("Problem playing file " + filename);
+            System.out.println(e);
+        }
+
+        // run in new thread to play in background
+        new Thread() {
+            public void run() {
+                try { player.play(); }
+                catch (Exception e) { System.out.println(e); }
+            }
+        }.start();
+
+
+
+
+    }
+
+
+    // test client
+    public static void main(String[] args) {
+ 
+        String filename = "data/music/laser.mp3";
+        MP3 mp3 = new MP3(filename);
+        mp3.play();
+
+        // do whatever computation you like, while music plays
+   
+
+        // when the computation is done, stop playing it
+      
+
+        // play from the beginning
+      
+
+    }
+
+}
+
